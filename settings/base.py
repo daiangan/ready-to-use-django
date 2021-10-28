0d26c762d8e4f6ee168abdb26157b75bc03de033
Daian Gan <daian@ganmedia.com>
2021-10-28 15:40:38 -0400

    Add django-cors-headers to requirements and base settings.

--- settings/base.py
+++ settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # 3rd party apps
     'django_cleanup',
     'django_s3_storage',
+    'corsheaders',
 
     # Project apps
     'utils',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
